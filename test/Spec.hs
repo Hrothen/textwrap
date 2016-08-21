0f578f0419ff6154f526adf18c3f9780517e06b0
Leif Grele <lgrele@gmail.com>
2016-08-21 09:07:09 -0400

    Initial test stubs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,43 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-
+Test suite for the textwrap module.
+
+Original tests written by Greg Ward <gward@python.net>.
+Converted to PyUnit by Peter Hansen <peter@engcorp.com>.
+Converted to Hspec by Leif Grele <lgrele@gmail.com>.
+Currently maintained by Leif Grele.
+-}
+
+
+module Main where
+import Test.Hspec
+import qualified Data.Text.Wrap as TW
+import Data.Text(Text)
+import qualified Data.Text as T
+
+main = hspec $ do
+  describe "Textwrap" $ do
+    wrapTests
+    maxLinesTests
+    longWordTests
+    indentTests
+    dedentTests
+    shortenTests
+
+
+wrapTests :: Spec
+wrapTests = undefined
+
+maxLinesTests :: Spec
+maxLinesTests = undefined
+
+longWordTests :: Spec
+longWordTests = undefined
+
+indentTests :: Spec
+indentTests = undefined
+
+dedentTests :: Spec
+dedentTests = undefined
+
+shortenTests :: Spec
+shortenTests = undefined
